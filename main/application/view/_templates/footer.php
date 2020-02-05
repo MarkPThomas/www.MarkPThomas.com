c65c45354160331b808bfc8033c16002afdba91a
Mark P Thomas <markums@gmail.com>
2020-02-05 13:10:59 -0700

    PWA-8: Improve basic page flowing for mobile: copyright vs. contact icons

--- main/application/view/_templates/footer.php
+++ main/application/view/_templates/footer.php
@@ -4,36 +4,42 @@
     <footer class="footer">
         <hr>
         <div class="row footer-logo-cr">
-            <div class="col-lg-12">
-                <div class="footer-content logo">
-                    <div class="footer-logo">
-                        <p>Copyright &copy; <?= date("Y"); ?> by Mark Thomas</p>
+            <div class="col-md-6 col-md-push-7">
+                <div class="row">
+                    <div class="col-md-6 sociallinks-footer">
+                    <!-- Generic Social Media -->
+                        <a href="https://www.facebook.com/profile.php?id=1234350" target="_blank" class="social-icon-wrapper w-inline-block" title="Facebook">
+                            <img src="<?= Config::get('URL'); ?>main/public/img/icons/facebook/facebook_icon.svg" alt="facebook logo">
+                        </a>
+                        <a href="https://www.instagram.com/pellucidwombato/" target="_blank" class="social-icon-wrapper w-inline-block" title="Instagram">
+                            <img src="<?= Config::get('URL'); ?>main/public/img/icons/instagram/2993766 - instagram logo media social.png" alt="instagram logo">
+                        </a>
+                        <a href="https://twitter.com/PellucidWombat" target="_blank" class="social-icon-wrapper w-inline-block" title="Twitter">
+                            <img src="<?= Config::get('URL'); ?>main/public/img/icons/twitter/twitter_icon.svg" alt="twitter logo">
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCH8u_4I-6iRoFEZvEb52nlg" target="_blank" class="social-icon-wrapper w-inline-block" title="YouTube">
+                            <img src="<?= Config::get('URL'); ?>main/public/img/icons/youTube/YouTube_social_red_squircle_(2017).svg" alt="youtube logo">
+                        </a>
+                        <div class="social-icon-wrapper w-inline-block"></div>
+                    </div>
+                    <div class="col-md-6 sociallinks-footer">
+                    <!-- Specific to sub-site-->
+                        <a href="https://www.linkedin.com/in/mark-thomas-599b598" target="_blank" class="social-icon-wrapper w-inline-block" title="LinkedIn">
+                            <img src="<?= Config::get('URL'); ?>main/public/img/icons/linkedIn/linkedIn_icon.svg" alt="linkedIn logo">
+                        </a>
+                        <a href="https://github.com/MarkPThomas" target="_blank" class="social-icon-wrapper w-inline-block" title="GitHub">
+                            <img src="<?= Config::get('URL'); ?>main/public/img/icons/gitHub/gitHub_icon.svg" alt="gitHub logo">
+                        </a>
+                        <a href="https://stackoverflow.com/users/3341503/pellucidwombat" target="_blank" class="social-icon-wrapper w-inline-block" title="StackOverflow">
+                            <img src="<?= Config::get('URL'); ?>main/public/img/icons/stackOverflow/stackOverflow_icon.png" alt="stackOverflow logo">
+                        </a>
                     </div>
                 </div>
             </div>
-            <div class="sociallinks-footer">
-                <a href="https://www.facebook.com/profile.php?id=1234350" target="_blank" class="social-icon-wrapper w-inline-block">
-                    <img src="<?= Config::get('URL'); ?>main/public/img/icons/facebook/facebook_icon.svg" alt="facebook logo">
-                </a>
-                <a href="https://www.instagram.com/pellucidwombato/" target="_blank" class="social-icon-wrapper w-inline-block">
-                    <img src="<?= Config::get('URL'); ?>main/public/img/icons/instagram/2993766 - instagram logo media social.png" alt="instagram logo">
-                </a>
-                <a href="https://twitter.com/PellucidWombat" target="_blank" class="social-icon-wrapper w-inline-block">
-                    <img src="<?= Config::get('URL'); ?>main/public/img/icons/twitter/twitter_icon.svg" alt="twitter logo">
-                </a>
-                <a href="https://www.youtube.com/channel/UCH8u_4I-6iRoFEZvEb52nlg" target="_blank" class="social-icon-wrapper w-inline-block">
-                    <img src="<?= Config::get('URL'); ?>main/public/img/icons/youTube/YouTube_social_red_squircle_(2017).svg" alt="youtube logo">
-                </a>
-                <div class="social-icon-wrapper w-inline-block"></div>
-                <a href="https://www.linkedin.com/in/mark-thomas-599b598" target="_blank" class="social-icon-wrapper w-inline-block">
-                    <img src="<?= Config::get('URL'); ?>main/public/img/icons/linkedIn/linkedIn_icon.svg" alt="linkedIn logo">
-                </a>
-                <a href="https://github.com/MarkPThomas" target="_blank" class="social-icon-wrapper w-inline-block">
-                    <img src="<?= Config::get('URL'); ?>main/public/img/icons/gitHub/gitHub_icon.svg" alt="gitHub logo">
-                </a>
-                <a href="https://stackoverflow.com/users/3341503/pellucidwombat" target="_blank" class="social-icon-wrapper w-inline-block">
-                    <img src="<?= Config::get('URL'); ?>main/public/img/icons/stackOverflow/stackOverflow_icon.png" alt="stackOverflow logo">
-                </a>
+            <div class="col-md-6 col-md-pull-6 footer-content logo">
+                <div class="footer-logo">
+                    <p>Copyright &copy; 2018-<?= date("Y"); ?> by Mark P. Thomas</p>
+                </div>
             </div>
             <!-- /.col-lg-12 -->
         </div>
